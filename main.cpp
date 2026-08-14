042b153c88b1c98f96fecdfe15d2300d6af36610
kdowd <kevin.dowd@gmail.com>
2026-08-14 12:12:39 +1200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include "nlohmann/json.hpp"
+
+using namespace std;
+using json = nlohmann::json;
+
+class LoadJSON {
+    public:
+    LoadJSON(string json_file_path) {
+    }
+};
+
+int main() {
+    LoadJSON theData("../data.json");
+    return 0;
+}
